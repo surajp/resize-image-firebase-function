93be776378a1ef2814a4701fdb66979202b1bb5c
Suraj Pillai <85.suraj@gmail.com>
2020-11-27 02:36:48 -0500

    Function for resizing images

---
+++ functions/index.js
@@ -0,0 +1,31 @@
+const functions = require("firebase-functions");
+const fetch = require("node-fetch");
+const sharp = require("sharp");
+
+const runtimeOpts = {
+  timeoutSeconds: 10,
+  memory: "1GB",
+};
+
+exports.resizeImage = functions
+  .runWith(runtimeOpts)
+  .https.onRequest(async (req, resp) => {
+    const imageUrl = req.query.url;
+    const width = parseInt(req.query.width) || 100;
+    const height = parseInt(req.query.height) || 40;
+    functions.logger.log("url", imageUrl);
+    fetch(imageUrl)
+      .then((res) => res.buffer())
+      .then((res) => {
+        return sharp(res).resize({ height, width, fit: "fill" }).toBuffer();
+      })
+      .then((data) => resp.json({ image: data.toString("base64") }))
+      .catch((err) => resp.json({ Error: err }));
+  });
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
