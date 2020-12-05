27712bb256b0e558be6fc78ea5c33e80dfcebf8c
Suraj Pillai <85.suraj@gmail.com>
2020-12-05 10:35:10 -0500

    Format response

--- functions/index.js
+++ functions/index.js
@@ -13,12 +13,23 @@ exports.resizeImage = functions
     const imageUrl = req.query.url;
     const width = parseInt(req.query.width) || 100;
     const height = parseInt(req.query.height) || 40;
+    let responseJson = {
+      originalImageUrl: imageUrl,
+      newWidth: width,
+      newHeight: height,
+    };
     functions.logger.log("url", imageUrl);
     fetch(imageUrl)
       .then((res) => res.buffer())
       .then((res) => {
         return sharp(res).resize({ height, width, fit: "fill" }).toBuffer();
       })
-      .then((data) => resp.json({ image: data.toString("base64") }))
-      .catch((err) => resp.json({ Error: err }));
+      .then((data) =>
+        resp.json(
+          Object.extend(responseJson, {
+            resizedImage: data.toString("base64"),
+          })
+        )
+      )
+      .catch((err) => resp.json(Object.extend(responseJson, { error: err })));
   });
