3bba88e12b50027dd813bf0262a997ca339462c1
Suraj Pillai <85.suraj@gmail.com>
2020-12-10 02:45:24 -0500

    Add pdf generator function

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require("firebase-functions");
 const fetch = require("node-fetch");
 const sharp = require("sharp");
+const html_to_pdf = require("html-pdf-node");
+const cors = require("cors")({ origin: true });
 
 const runtimeOpts = {
   timeoutSeconds: 10,
@@ -9,27 +11,42 @@ const runtimeOpts = {
 
 exports.resizeImage = functions
   .runWith(runtimeOpts)
-  .https.onRequest(async (req, resp) => {
-    const imageUrl = req.query.url;
-    const width = parseInt(req.query.width) || 100;
-    const height = parseInt(req.query.height) || 40;
-    let responseJson = {
-      originalImageUrl: imageUrl,
-      newWidth: width,
-      newHeight: height,
-    };
-    functions.logger.log("url", imageUrl);
-    fetch(imageUrl)
-      .then((res) => res.buffer())
-      .then((res) => {
-        return sharp(res).resize({ height, width, fit: "fill" }).toBuffer();
-      })
-      .then((data) =>
-        resp.json(
-          Object.extend(responseJson, {
-            resizedImage: data.toString("base64"),
-          })
+  .https.onRequest((req, resp) => {
+    cors(req, resp, async () => {
+      const imageUrl = req.query.url;
+      const width = parseInt(req.query.width) || 100;
+      const height = parseInt(req.query.height) || 40;
+      let responseJson = {
+        originalImageUrl: imageUrl,
+        newWidth: width,
+        newHeight: height,
+      };
+      functions.logger.log("url", imageUrl);
+      fetch(imageUrl)
+        .then((res) => res.buffer())
+        .then((res) => {
+          return sharp(res).resize({ height, width, fit: "fill" }).toBuffer();
+        })
+        .then((data) =>
+          resp.json(
+            Object.extend(responseJson, {
+              resizedImage: data.toString("base64"),
+            })
+          )
         )
-      )
-      .catch((err) => resp.json(Object.extend(responseJson, { error: err })));
+        .catch((err) => resp.json(Object.extend(responseJson, { error: err })));
+    });
   });
+
+exports.genPdf = functions.runWith(runtimeOpts).https.onRequest((req, resp) => {
+  cors(req, resp, async () => {
+    let options = { format: "A4" };
+    let file = { content: req.body.content };
+    try {
+      let pdfBuffer = await html_to_pdf.generatePdf(file, options);
+      resp.json({ data: pdfBuffer.toString("base64") });
+    } catch (err) {
+      resp.json({ error: err });
+    }
+  });
+});
